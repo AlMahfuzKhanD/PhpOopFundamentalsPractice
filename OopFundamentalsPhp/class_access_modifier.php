e0dbb008ef758128e451a51980062a5374eb17ef
Al Mahfuz <almahfuz380@gmail.com>
2019-01-22 12:18:44 +0600

    class inheritance and access modifier done

---
+++ OopFundamentalsPhp/class_access_modifier.php
@@ -0,0 +1,79 @@
+<?php include "functions.php" ?>
+<?php include "includes/header.php" ?>
+
+	<section class="content">
+
+		<aside class="col-xs-4">
+
+	<?php Navigation();?>
+			
+			
+		</aside><!--SIDEBAR-->
+
+
+		<article class="main-content col-xs-8">
+		
+
+
+		<?php
+
+		/* Step 1: Make 2 variables called number1 and number2 and set 1 to value 10 and the other 20:
+
+		  Step 2: Add the two variables and display the sum with echo:
+
+
+		  Step3: Make 2 Arrays with the same values, one regular and the other associative
+
+		  Step4: Make a constant and set it to the value of PHP. and use an echo to print it out
+
+
+			
+
+			 */
+
+		//class access Modifier
+
+        class Cars{
+            public $wheels = 4;
+            Private $door_count = 2;
+            Protected $seat_count = 6;
+
+
+            function car_detail(){
+
+echo $this->wheels;
+echo $this->door_count;
+echo $this->seat_count;
+
+
+            }
+
+
+        }
+        $bmw = new Cars();
+        //echo $bmw->wheels;
+        //echo $bmw->door_count;
+        //echo $bmw->seat_count;
+       $bmw->car_detail();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		?>
+
+	
+
+		</article><!--MAIN CONTENT-->
+
+<?php include "includes/footer.php" ?>
